1a4e410dd75fed23a197bf722670b2423786d1a2
xperrin <xperrin@student.42.fr>
2017-11-11 14:55:38 +0100

    strlcat draft, memmove fixed

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,23 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: xperrin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/05 19:10:02 by xperrin           #+#    #+#             */
-/*   Updated: 2017/11/11 14:09:49 by xperrin          ###   ########.fr       */
+/*   Created: 2017/11/11 14:35:32 by xperrin           #+#    #+#             */
+/*   Updated: 2017/11/11 14:49:01 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	void	*tmp;
+	int		i;
+	size_t	e;
 
-	tmp = ft_memalloc(len);
-	tmp = ft_memcpy(tmp, src, len);
-	ft_memcpy(dst, tmp, len);
-	return (dst);
+	e = -1;
+	i = ft_strlen(dst) - 1;
+	while (src[++e] && e < size)
+		dst[++i] = src[e];
+	dst[++i] = '\0';
+	return (e);
 }
