fe6a134fa8d231e35d7b554306a17fe34e889f33
xperrin <xperrin@student.42.fr>
2017-11-17 02:42:38 +0100

    strstr and strnstr improvments

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 18:22:38 by xperrin           #+#    #+#             */
-/*   Updated: 2017/11/16 18:31:43 by xperrin          ###   ########.fr       */
+/*   Updated: 2017/11/17 02:38:40 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,16 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	unsigned int	needle_len;
-	unsigned int	i;
-	unsigned int	index;
-	unsigned int	tmp;
 
-	i = 0;
-	index = 0;
 	if (!(*needle))
 		return ((char*)haystack);
 	needle_len = ft_strlen(needle);
-	while (*haystack && index < len)
+	while (*haystack && needle_len <= len--)
 	{
-		tmp = index;
-		while (haystack[i] == needle[i] && tmp < len)
-		{
-			i++;
-			tmp++;
-			if (i == needle_len)
+		if (*haystack == *needle
+				&& !(ft_memcmp(haystack, needle, needle_len)))
 				return ((char*)haystack);
-		}
-		i = 0;
 		haystack++;
-		index++;
 	}
 	return (NULL);
 }
--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 21:30:11 by xperrin           #+#    #+#             */
-/*   Updated: 2017/11/16 18:16:03 by xperrin          ###   ########.fr       */
+/*   Updated: 2017/11/17 02:41:29 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,5 @@
 
 char	*ft_strstr(const char *haystack, const char *needle)
 {
-	unsigned int	needle_len;
-	unsigned int	i;
-
-	i = 0;
-	if (!(*needle))
-		return ((char*)haystack);
-	needle_len = ft_strlen(needle);
-	while (*haystack)
-	{
-		while (haystack[i] == needle[i])
-		{
-			i++;
-			if (i == needle_len)
-				return ((char*)haystack);
-		}
-		i = 0;
-		haystack++;
-	}
-	return (NULL);
+	return (ft_strnstr(haystack, needle, ft_strlen(haystack)));
 }
