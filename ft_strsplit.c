7a79e0eb7ef76258b57d592773916b0b495ea381
xperrin <xperrin@student.42.fr>
2017-11-23 12:28:35 +0100

    strsplit bugfix

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/22 10:15:21 by xperrin           #+#    #+#             */
-/*   Updated: 2017/11/22 21:43:24 by xperrin          ###   ########.fr       */
+/*   Updated: 2017/11/23 12:10:55 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char		**ft_strsplit(char const *s, char c)
 	wc = ft_cntword(s, c);
 	if (!(dst = (char**)ft_memalloc(sizeof(char*) * (wc + 1))))
 		return (NULL);
+	if (!ft_strlen(s))
+		return (dst);
 	wi = -1;
 	while (++wi < wc)
 	{
