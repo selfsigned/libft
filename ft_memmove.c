21982e9a8c606b4086225676842744be0a29cd60
xperrin <xperrin@student.42.fr>
2017-11-24 23:15:04 +0100

    memmove without malloc

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/05 19:10:02 by xperrin           #+#    #+#             */
-/*   Updated: 2017/11/11 14:09:49 by xperrin          ###   ########.fr       */
+/*   Updated: 2017/11/24 23:14:10 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	void	*tmp;
-
-	tmp = ft_memalloc(len);
-	tmp = ft_memcpy(tmp, src, len);
-	ft_memcpy(dst, tmp, len);
+	if (dst < src)
+		return ft_memcpy(dst, src, len);
+	while (len--)
+		((char*)dst)[len] = ((char*)src)[len];
 	return (dst);
 }
