ab1a1abc927052b33b140de2c4585de545d8b50e
xperrin <xperrin@student.42.fr>
2017-11-25 12:00:33 +0100

    ft_lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/25 11:17:13 by xperrin           #+#    #+#             */
+/*   Updated: 2017/11/25 11:57:40 by xperrin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*new;
+
+	if (!(new = ft_memalloc(sizeof(*new))))
+		return (NULL);
+	if (!content)
+	{
+		new->content = NULL;
+		new->content_size = 0;
+		free(new);
+	}
+	else
+	{
+		if (!(new->content = ft_memalloc(content_size)))
+		{
+			free (new);
+			return (NULL);
+		}
+		ft_memcpy(new->content, content, content_size);
+		new->content_size = content_size;
+	}
+	new->next = NULL;
+	return (new);
+}
