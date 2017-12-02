15f61b94c38ed6311edbe02e67ac340e72fc44f4
xperrin <xperrin@student.42.fr>
2017-12-02 01:01:23 +0100

    Programming the flux capacitor

--- src/ft_lstnew.c
+++ src/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/25 11:17:13 by xperrin           #+#    #+#             */
-/*   Updated: 2017/11/28 20:48:32 by xperrin          ###   ########.fr       */
+/*   Updated: 2017/12/02 01:00:39 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*new;
 
-	if (!(new = ft_memalloc(sizeof(*new))))
+	if (!(new = (t_list*)ft_memalloc(sizeof(*new))))
 		return (NULL);
 	if (!content)
 	{
